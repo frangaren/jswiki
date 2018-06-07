146249e5faaeb8f962721bf47b4fe5026a5ed611
Francisco García <frangaren@usal.es>
2018-06-08 00:49:53 +0200

    Add user model

---
+++ model/user.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: String,
+    email: String,
+    name: String,
+    password: String
+});
+
+userSchema.path('username').required(true).unique(true);
+userSchema.path('email').required(true).unique(true);
+userSchema.path('name').required(true);
+userSchema.path('password').required(true).select(false);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
