63c6d9fe069667ba5963ad4f82c177c68c88654c
Francisco García <frangaren@usal.es>
2018-06-10 01:48:18 +0200

    Add article list

--- static/scripts/components/article-short.js
+++ static/scripts/components/article-short.js
@@ -3,8 +3,35 @@ Vue.component('article-short', {
         'value'
     ],
     template: `
-        <div class="article">
-            <h3>{{value.topic}}</h3>
+        <div class="article-short">
+            <h2>{{value.topic}}</h2>
+            <div class="article-categories">
+                <category class="chip" v-for="category in categories" :value="category" 
+                    :key="category._id" no-controls="true"/>
+            </div>
         </div>
     `,
+    data: function() {
+        return {
+            categories: []
+        };
+    },
+    created: function () {
+        this.load();
+    },
+    watch: {
+        value: function() {
+            this.categories = [];
+            this.load();
+        }
+    },
+    methods: {
+        load: function() {
+            for (category of this.value.categories) {
+                axios.get(`/api/v1/categories/${category}`)
+                    .then(res => this.categories.push(res.data))
+                    .catch(console.error);
+            }
+        }
+    }
 });
