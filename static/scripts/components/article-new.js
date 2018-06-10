54c74cbb0ff4e6eb476f6881397d58227c2a7ec4
Francisco García <frangaren@usal.es>
2018-06-10 13:09:57 +0200

    Add new article page

--- static/scripts/components/article-new.js
+++ static/scripts/components/article-new.js
@@ -1,7 +1,26 @@
 Vue.component('article-new', {
     template: `
-        <div class="container">
-            <article-editor/>
+        <div class="article-new container">
+            <article-editor v-model="article" @input="onSave"/>
         </div>
     `,
+    data: function () {
+        return {
+            article: {
+                topic: 'Nuevo artículo',
+                body: '',
+                categories: []
+            }
+        }
+    },
+    methods: {
+        onSave: function () {
+            axios.post(`/api/v1/articles`, this.article)
+                .then(res => {
+                    this.article = res.data;
+                    router.push(`/article/${this.article._id}`);
+                })
+                .catch(console.error);
+        }
+    }
 });
